187aaf11a2a6edae4a073a41646f6ab43660c537
pks10 <mstr44872@gmail.com>
2026-01-16 00:51:56 +0530

    guess_number

--- guess_number.py
+++ guess_number.py
@@ -1,1 +1,17 @@
-x=input("enter lower range :")
+import random
+x=int(input("enter lower range :"))
+y=int(input("enter higher range:"))
+k=random.randint(x,y)
+print(k)
+
+def guess():
+    z=int(input("enter the number you have guessed:"))
+    if z!=k:
+        print("entered number is not the guessed number")
+        guess()
+    else:
+        print("you have guessed correctly")
+        exit()
+
+guess()
+
